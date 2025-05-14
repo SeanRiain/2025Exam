275ec69cccb7c58f8ff1605fc1b2f37a8f377b52
SeanRiain <S00254331@atu.ie>
2025-05-14 11:04:09 +0100

    Listbox Populated

--- PatientAppointments/MainWindow.xaml.cs
+++ PatientAppointments/MainWindow.xaml.cs
@@ -20,9 +20,24 @@ namespace PatientAppointments
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PatientData PatientData = new PatientData();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Query the database for all patients
+        //Order them by Name
+        //Display in listbox in the formated designated by the ToString()
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var PatientQuery = from SinglePatient in PatientData.ListOfPatients
+                               where SinglePatient.PatientID > 0 //Should select all Patients as all their IDs will be above zero
+                               select SinglePatient;
+
+            var Patients = PatientQuery.ToList(); 
         }
 
        
